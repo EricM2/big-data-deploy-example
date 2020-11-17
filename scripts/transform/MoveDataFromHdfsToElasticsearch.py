c9c3a1a0d6c6b36358df38559155a1eb14037c9c
EricM <guyericmodjo@gmail.com>
2020-11-17 12:17:19 +0100

    first

---
+++ scripts/transform/MoveDataFromHdfsToElasticsearch.py
@@ -0,0 +1,6 @@
+def main():
+    print("This is the script witch move data from the hdfs to elasticsearch")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/main/scala/com/tpm/TweetPerformanceMotor.scala
@@ -0,0 +1,7 @@
+package com.tpm
+
+object TweetPerformanceMotor {
+    def main ( args : Array[String]): Unit ={
+      print("This is the Spark tweet performance motor...")
+    }
+}
